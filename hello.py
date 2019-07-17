008c5d5126ff3f4ac2782363c9ec203fe7d28d4a
Timo Kohlmann <timo.kohlmann@jvm.de>
2019-07-17 11:04:54 +0200

    add more python files

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-for i in range(21):
-    print(i)
+def square(x):
+    return x * x
+
+def main():
+    for i in range(21):
+        print("{} squared is {}".format(i, square(i)))
+
+if __name__ == "__main__":
+    main()
